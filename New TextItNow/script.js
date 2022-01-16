408898d951995374afdd9c54c78aef218679a4fc
Benjamin Burnell <70352588+bennyboy21@users.noreply.github.com>
2022-01-15 23:36:58 -0500

    Update script.js

--- New TextItNow/script.js
+++ New TextItNow/script.js
@@ -1,5 +1,12 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName())
+    $("#email").text(profile.geEmail())
+    $("#image").text("src", profile.getImageUrl())
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "none");
+    
+    var profile = googleUser.getBasicProfile();
     console.log(profile)
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
