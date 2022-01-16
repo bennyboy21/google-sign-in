da96f2253ebc8e34a0e5076c4f142446651b5be8
Benjamin Burnell <70352588+bennyboy21@users.noreply.github.com>
2022-01-15 23:20:55 -0500

    Update script.js

--- New TextItNow/script.js
+++ New TextItNow/script.js
@@ -1,8 +1,21 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName())
-    $("#email").text(profile.geEmail())
-    $("#image").text(profile.getImageUrl())
+    console.log(profile)
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    document.querySelector(".data").style.display = "block"
+    document.querySelector(".g-signin2").style.display = "none"
+    const allowedEmailDomain = 'student.tdsb.on.ca';
+
+    const email = profile.getEmail();
+
+    if (email.split('@')[1] === allowedEmailDomain) {
+        alert("we allow this email")
+    } else {
+        alert("We don't allow this email")
+    }
     $(".data").css("display", "block");
     $(".g-signin2").css("display", "none");
 }
