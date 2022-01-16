fe3d485d959c52a88ddd020d4b48e28c04ac205e
Benjamin Burnell <70352588+bennyboy21@users.noreply.github.com>
2022-01-15 23:53:28 -0500

    Update script.js

--- New TextItNow/script.js
+++ New TextItNow/script.js
@@ -28,5 +28,6 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       alert('User signed out.')
+//         window.reload();
     });
 }
